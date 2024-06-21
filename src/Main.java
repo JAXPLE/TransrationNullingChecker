26166e04a98e1a06f2fea93992052df49642db14
JwonLee <114869036+JAXPLE@users.noreply.github.com>
2024-06-21 10:11:27 +0900

    Now Support Legacy DataFile Add New Data

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,70 @@
+import javax.xml.crypto.Data;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        File originLanguage = new File("C:\\Users\\Jwon0\\IdeaProjects\\TransrationNullingChecker\\src\\en_us.json");
-        File targetLanguage = new File("C:\\Users\\Jwon0\\IdeaProjects\\TransrationNullingChecker\\src\\ko_kr.json");
+        File originLanguage = new File("src/en_us.json");
+        File targetLanguage = new File("src/ko_kr.json");
 
-        ArrayList<String> originDataGettering = trimmingData(originLanguage);
-        ArrayList<String> targetDataGettering = trimmingData(targetLanguage);
+        ArrayList<String[]> originDataGettering = trimmingData(originLanguage);
+        ArrayList<String[]> targetDataGettering = trimmingData(targetLanguage);
 
-        missingDataIndex(originDataGettering,targetDataGettering);
+        ArrayList<String> totalData = missingDataIndex(originDataGettering,targetDataGettering);
+        addMessage(totalData);
     }
 
-    private static void missingDataIndex(ArrayList<String> original, ArrayList<String> target) {
-        ArrayList<Boolean> resultList = new ArrayList<>();
-        for (String originData : original) {
-            boolean existChecker = false;
-            for (String targetData : target) {
-                if (originData.equals(targetData)) {
-                    existChecker = true;
+    private static ArrayList<String> missingDataIndex(ArrayList<String[]> original, ArrayList<String[]> target) {
+//        ArrayList<Boolean> resultList = new ArrayList<>();
+        ArrayList<String> resultList = new ArrayList<>();
+
+        for (String[] originData : original) {
+            if (originData.length < 2)
+                continue;
+
+            String data = originData[1];
+            for (String[] targetData : target) {
+                if (originData[0].equals(targetData[0])) {
+                    data = targetData[1];
                     break;
                 }
             }
-            resultList.add(existChecker);
+            resultList.add(originData[0] + ": " + data +",\n");
         }
-        System.err.println("Missing Key Value ↓↓↓↓↓↓↓↓↓↓");
-        for (int i = 0; i < resultList.size(); i++)
-            if (!resultList.get(i)) System.out.println(original.get(i));
+
+        System.out.println(resultList);
+//        for (int i = 0; i < resultList.size(); i++)
+//            if (!resultList.get(i))
+//                System.err.println(original.get(i)[0]);
+        return resultList;
     }
 
-    private static ArrayList<String> trimmingData(File targetFile) throws Exception {
-        ArrayList<String> resultValue = new ArrayList<>();
+    private static void addMessage(ArrayList<String> totalData) {
+        try(FileWriter writer = new FileWriter("KR.json")) {
+            for (String data : totalData) {
+                writer.write(data);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static ArrayList<String[]> trimmingData(File targetFile) throws Exception {
+        ArrayList<String[]> resultValue = new ArrayList<>();
         BufferedReader bufferedReader = new BufferedReader(new FileReader(targetFile));
         while (bufferedReader.ready()) {
-            String[] key = bufferedReader.readLine().split(":");
-            String[] keyValue = key[0].split("\"");
+            String[] data = bufferedReader.readLine().split(",");
+//            System.out.println(Arrays.toString(data));
+            String[] keyValue = data[0].split(":");
+//            keyValue
 
-            if (keyValue.length < 2)
-                continue;
-
-            resultValue.add(keyValue[1]);
+            resultValue.add(keyValue);
         }
 
         return resultValue;
